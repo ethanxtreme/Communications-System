26d3d96fa241191f8e3047bdab26ba5784d5ae78
Ethan Neves <ethan4083@gmail.com>
2023-04-19 16:06:25 -0700

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,6 +4,6 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("ur mom stinks");
+	//github desktop test - Ethan
 	}
 }
