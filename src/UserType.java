2f09f44c5f275b872828b21dd0004874ce7bdbe2
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 21:07:44 -0700

    Create UserType.java
    Created enumeration for user type (see comment)

---
+++ src/UserType.java
@@ -0,0 +1,7 @@
+
+// not sure if we discussed whether or not to use this, but I'm adding it here just in case
+// and so that I can use it in the User.java class
+
+public enum UserType {
+	USER, ADMIN;
+}
