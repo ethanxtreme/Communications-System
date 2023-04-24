b855ebb990844b8aa618955571adcaac859c377d
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 20:41:33 -0700

    Create ConsoleUI.java
    created driver for console application

---
+++ src/ConsoleUI.java
@@ -0,0 +1,11 @@
+// TODO don't start until a rough GUI outline has been created
+
+
+public class ConsoleUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
