bfcd460821d2a3ab1d8cc741780fde77600bcc0c
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 21:28:08 -0700

    Added other methods
    Added methods: login, message, createGroup, viewLogs (see comment). Also fixed typo in constructor

--- src/User.java
+++ src/User.java
@@ -19,12 +19,34 @@ public class User {
 		chatGroups = new ChatGroup[DEFAULT_GROUPS];
 	}
 	
-	public User(String userId, String username, UserType userType, String password, Chatgroup[] chatGroups) {
+	public User(String userId, String username, UserType userType, String password, ChatGroup[] chatGroups) {
 		this.userId = userId;
 		this.username = username;
 		this.userType = userType;
 		this.password = password;
 		this.chatGroups = chatGroups;
+	}
+	
+	public void login(String userId, String password) {
+		
+	}
+	
+	// return type may need to be NetworkMessage, but for now void seems to make more sense
+	public void message(NetworkMessage message, String recipientUsername) { 
+		
+	}
+	
+	public ChatGroup createGroup(User[] users, String name) {
+		
+		
+		return null;
+	}
+	
+	// should only be accessible if user is an ADMIN
+	// for a GUI application, it's less necessary to code the UserType check in this class,
+	// but rather have the option only available on screen in user is an ADMIN
+	public void viewLogs() {
+		
 	}
 	
 }
