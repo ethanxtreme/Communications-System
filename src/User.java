e71a16b0995646f546b63e347901235ab7583516
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 21:37:37 -0700

    Added debug messages
    Added messages to print to console to ensure class was called correctly.

--- src/User.java
+++ src/User.java
@@ -29,15 +29,25 @@ public class User {
 	
 	public void login(String userId, String password) {
 		
+		// TODO debug message
+		System.out.println("Login with userID: " + userId + " and password " + password);
 	}
 	
 	// return type may need to be NetworkMessage, but for now void seems to make more sense
 	public void message(NetworkMessage message, String recipientUsername) { 
 		
+		// TODO debug message
+		System.out.println("Message reading \"" + message + "\" sent to " + recipientUsername);
 	}
 	
 	public ChatGroup createGroup(User[] users, String name) {
 		
+		// TODO debug message
+		System.out.print("Group named \"" + name + "\" created with users:");
+		for (int i=0; i<users.length; i++) {
+			if (users[i] == null) {break;}
+			System.out.println(users[i]);
+		}
 		
 		return null;
 	}
@@ -47,6 +57,8 @@ public class User {
 	// but rather have the option only available on screen in user is an ADMIN
 	public void viewLogs() {
 		
+		// TODO debug message
+		System.out.println("View Logs successfully called.");
 	}
 	
 }
