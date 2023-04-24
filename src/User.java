dc1f2b8e583cad0846bb5f10d805ad234bd68624
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 21:07:28 -0700

    Update User.java
    Added constructors to user class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,30 @@
 
 public class User {
 
+	// default value for number of chat groups allowed at first
+	private final int DEFAULT_GROUPS = 8;
+	
+	private String userId;
+	private String username;
+//	private UserType usertype;
+	private String password;
+	private ChatGroup[] chatGroups;
+	
+	// default constructor
+	public User() {
+		userId = "";
+		username = "";
+		userType = UserType.USER; 
+		password = "";
+		chatGroups = new ChatGroup[DEFAULT_GROUPS];
+	}
+	
+	public User(userId, username, userType password, chatGroups) {
+		this.userId = userId;
+		this.username = username;
+		this.usertype = userType;
+		this.password = password;
+		this.chatGroups = chatGroups;
+	}
+	
 }
