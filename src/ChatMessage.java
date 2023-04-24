72594cb7186e7e1fa7846c139f0a62f556f908f1
jesse-with-no-i <jcometa11@gmail.com>
2023-04-23 23:16:48 -0700

    Added simple constructor and getter
    needed to add these for testing purposes

--- src/ChatMessage.java
+++ src/ChatMessage.java
@@ -1,4 +1,15 @@
 
 public class ChatMessage {
-
+	
+	private String text;
+	
+	// constuctors
+	public ChatMessage(String text) {
+		this.text = text;
+	}
+	
+	// getters
+	public String getText() {
+		return text;
+	}
 }
