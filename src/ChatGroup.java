807b23de4e58ceb2525e51bd3d3ca58308ec11b3
Samantha Bustamante <60456791+sam-24-bot@users.noreply.github.com>
2023-04-25 14:02:19 -0700

    Updated ChatGroup.java

--- src/ChatGroup.java
+++ src/ChatGroup.java
@@ -1,18 +1,38 @@
+import java.util.*;
 
 public class ChatGroup {
 	public String groupName;
-	public User[] members;
-	private ChatLog chatlog;
+	public List<Thread> members;
+	private ChatLog log_obj;
 	
-	public void addUser(String userId) {
-		
+	public ChatGroup() {
+		members = new ArrayList<Thread>();
+		setgroupname(groupName);
 	}
 	
-	public void removeUser(String userId) {
-		
+	public void setgroupname(String groupName) {
+		this.groupName = groupName;
+	}
+	public String getgroupname() {
+		return groupName;
 	}
 	
-	public String sendGroupMessage(String message) {
+	public void addUser(Thread userId) {
+		members.add(userId);
+	}
+	
+	public void removeUser(Thread userId) {
+		for(int i = 0; i < members.size(); i++) {
+			if(members.get(i).equals(userId)) {
+				members.remove(i);
+			}
+		}
+	}
+	
+	public Thread sendGroupMessage(Thread message) {
+		log_obj = new ChatLog();
+		log_obj.chatLog.add(message);
+		return message;
 		
 	}
 
