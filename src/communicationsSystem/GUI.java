64360c563db8ce1de6ed8714f4bf9338a1280641
jesse-with-no-i <jcometa11@gmail.com>
2023-04-28 02:56:29 -0700

    GUI.java --> Login window
    added a (non-functional) login window GUI

--- src/communicationsSystem/GUI.java
+++ src/communicationsSystem/GUI.java
@@ -3,39 +3,70 @@ package communicationsSystem;
 
 // Communications system GUI 
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class GUI {
 	public static void main(String[] args) {
-	      createWindow();
+	      createLoginWindow();
 	   }
 	
-	private static void createWindow() {    
-	      JFrame frame = new JFrame("GUI");
+	private static void createLoginWindow() {    
+	      JFrame frame = new JFrame("Log in");
 	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	      createUI(frame);
-	      frame.setSize(720, 480);      
+	      
+	      createLoginUI(frame);
+	      frame.setSize(360, 200);      
 	      frame.setLocationRelativeTo(null);  // Center on screen
 	      frame.setVisible(true);	// make visible
 	   }
 	
-	private static void createUI(final JFrame frame){  
-	      JPanel panel = new JPanel();
-	      LayoutManager layout = new FlowLayout();  // comment out: To try alternate layouts
-	      //LayoutManager layout = new GridLayout();  // uncomment: To try alternate layouts
-	      panel.setLayout(layout);       
+	private static void createLoginUI(final JFrame frame){  
+	      JPanel panel = new JPanel(new GridBagLayout());
+	     
+	      JLabel usernameLabel = new JLabel("Username:");
+	      JTextField usernameText = new JTextField(20); // create a textfield for entering username
+	      
+	      JLabel passwordLabel = new JLabel("Password:");
+	      JPasswordField passwordText = new JPasswordField(20); // textfield for password
+	      JButton loginButton = new JButton("Log In");
+	      JButton exitButton = new JButton("Exit");
+	      
+	      GridBagConstraints constraints = new GridBagConstraints();
+	      constraints.anchor = GridBagConstraints.WEST;
+	      constraints.insets = new Insets(10, 10, 10, 10);
+	      
+	      constraints.gridx = 0;
+	      constraints.gridy = 0;     
+	      panel.add(usernameLabel, constraints);
+	      
+	      constraints.gridx = 1;     
+	      panel.add(usernameText, constraints);
+	      
+	      constraints.gridx = 0;
+	      constraints.gridy = 1;
+	      panel.add(passwordLabel, constraints);
+	      
+	      constraints.gridx = 1;
+	      panel.add(passwordText, constraints);
+	      
+	      constraints.gridx = 0;
+	      constraints.gridy = 2;
+	      panel.add(loginButton, constraints);
+	      
+	      constraints.gridx = 1;
+	      panel.add(exitButton, constraints);
+	      
+	      
+	   
+	      
+//	      frame.getRootPane().setDefaultButton(); //Default button focus
+
+	      
+	      frame.getContentPane().add(panel, BorderLayout.CENTER); 
 	}
+	
+	
 }
