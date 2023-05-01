ea6bbcbfa6122f5361a0142500a5493de88075e1
Ethan Neves <ethan4083@gmail.com>
2023-04-30 18:04:52 -0700

    Initial Commit for tests

---
+++ src/Testing/TestSuite.java
@@ -0,0 +1,14 @@
+package Testing;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	
+	UserTester.class//GroupTester.class, etc...
+	
+})
+public class TestSuite {
+}
---
+++ src/Testing/UserTester.java
@@ -0,0 +1,26 @@
+package Testing;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.RepeatedTest;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import communicationsSystem.User;
+
+public class UserTester {
+	@Test
+	@RepeatedTest(10)
+    public void test1() {
+        //not implemented, please rename test functions as well
+    }
+	
+	@RepeatedTest(10)
+    public void test2() {
+        
+    }
+	
+	@Test
+	public void test3() {
+		
+	}
+}
+
