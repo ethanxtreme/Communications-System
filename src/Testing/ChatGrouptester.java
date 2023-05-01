271b984d549daf0c468af46f01a96f23b93beaf6
Samantha Bustamante <60456791+sam-24-bot@users.noreply.github.com>
2023-04-30 22:00:42 -0700

    Updated Tests

--- src/Testing/ChatGrouptester.java
+++ src/Testing/ChatGrouptester.java
@@ -1,34 +1,66 @@
 package Testing;
 import communicationsSystem.ChatGroup;
+import communicationsSystem.User;
+import communicationsSystem.UserType;
+import communicationsSystem.Thread;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import java.util.*;
 
-class ChatGrouptester {
+public class ChatGrouptester {
+	
+	String groupname = "Group1";
 
 	@Test
-	void testSetgroupname() {
-		fail("Not yet implemented");
+	public void testSetgroupname() {
+		assertEquals("Groupname", groupname);
 	}
 
 	@Test
-	void testGetgroupname() {
-		fail("Not yet implemented");
+	public void testGetgroupname() {
+		ChatGroup group = new ChatGroup();
+		group.setgroupname(groupname);
+		assertEquals("Groupname", group.getgroupname());
+		
 	}
 
 	@Test
-	void testAddUser() {
-		fail("Not yet implemented");
+	public void testAddUser() {
+		boolean trueBool = true;
+		boolean falseBool = false;
+		UserType user_1 = null;
+		User user = new User("1", "sb55", user_1.USER, "12345");
+		ArrayList<User> user_list = new ArrayList<User>();
+		user_list.add(user);
+		if(user_list.contains(user)) {
+			assertTrue(trueBool);
+		}
+		else {
+			assertTrue(falseBool);
+		}
 	}
 
 	@Test
-	void testRemoveUser() {
-		fail("Not yet implemented");
+	public void testRemoveUser() {
+		boolean trueBool = true;
+		boolean falseBool = false;
+		UserType user_1 = null;
+		User user = new User("1", "sb55", user_1.USER, "12345");
+		ArrayList<User> user_list = new ArrayList<User>();
+		user_list.remove(user);
+		if(!user_list.contains(user)) { //meaning the user was successfully removed
+			assertTrue(trueBool);
+		}
+		else { //user was not successfully removed
+			assertTrue(falseBool);
+		}
 	}
 
 	@Test
-	void testSendGroupMessage() {
+	public void testSendGroupMessage() {
 		fail("Not yet implemented");
 	}
 
