7a00b25aed935b8d3d8822aa2918e3fa87cf2fd6
Samantha Bustamante <60456791+sam-24-bot@users.noreply.github.com>
2023-05-01 00:05:32 -0700

    Updated ChatMessage Tests

--- src/Testing/ChatMessagetester.java
+++ src/Testing/ChatMessagetester.java
@@ -1,65 +1,97 @@
 package Testing;
 import communicationsSystem.ChatMessage;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Date;
 
-class ChatMessagetester {
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
+public class ChatMessagetester {
+	Date test_date = new Date();
+    String[] test_recipentIDs = {"a", "b", "c"};
+    ChatMessage message = new ChatMessage("123", "efgh",test_recipentIDs,"Hello", test_date);
+    String[] emptyrecipentIDs = {"", "", ""};
+	ChatMessage test_message = new ChatMessage("", "",emptyrecipentIDs, "", null);
 
 	@Test
-	void testChatMessage() {
-		fail("Not yet implemented");
+	public void testChatMessage() {
+        assertEquals("123", message.getMessageId());
+        assertEquals("efgh", message.getSenderId());
+        assertEquals(test_recipentIDs, message.getRecipientIds());
+        assertEquals("Hello", message.getMessageText());
+        assertEquals(test_date, message.getTimeStamp());
 	}
 
 	@Test
-	void testGetMessageId() {
-		fail("Not yet implemented");
+	public void testGetMessageId() {
+		System.out.println("test getMessageID:" + message.getMessageId());
+		assertEquals("123", message.getMessageId());
 	}
 
 	@Test
-	void testGetSenderId() {
-		fail("Not yet implemented");
+	public void testGetSenderId() {
+		System.out.println("test getSenderID:" + message.getSenderId());
+		assertEquals("efgh", message.getSenderId());
 	}
 
 	@Test
-	void testGetRecipientIds() {
-		fail("Not yet implemented");
+	public void testGetRecipientIds() {
+		System.out.println("test getRecipentID:" + message.getRecipientIds());
+		assertEquals(test_recipentIDs, message.getRecipientIds());
 	}
 
 	@Test
-	void testGetMessageText() {
-		fail("Not yet implemented");
+	public void testGetMessageText() {
+		System.out.println("test getMesageText:" + message.getMessageText());
+		assertEquals("Hello", message.getMessageText());
 	}
 
 	@Test
-	void testGetTimeStamp() {
-		fail("Not yet implemented");
+	public void testGetTimeStamp() {
+		System.out.println("test gettimestamp:" + message.getTimeStamp());
+		assertEquals(test_date, message.getTimeStamp());
 	}
 
 	@Test
-	void testSetMessageId() {
-		fail("Not yet implemented");
+	public void testSetMessageId() {
+		String messageID = "456";
+		test_message.setMessageId(messageID);
+		System.out.println("test setmessageid" + test_message.getMessageId());
+		assertEquals("456", test_message.getMessageId());
 	}
 
 	@Test
-	void testSetSenderId() {
-		fail("Not yet implemented");
+	public void testSetSenderId() {
+		String senderID = "hij";
+		test_message.setMessageId(senderID);
+		System.out.println("test setsenderid" + test_message.getSenderId());
+		assertEquals("hij", test_message.getSenderId());
 	}
 
 	@Test
-	void testSetRecipientIds() {
-		fail("Not yet implemented");
+	public void testSetRecipientIds() {
+		String[] populatedrecipetIDs = {"a", "b", "c"};
+		test_message.setRecipientIds(populatedrecipetIDs);
+		System.out.println("test setrecipientIDs" + test_message.getRecipientIds());
+		assertEquals(populatedrecipetIDs, test_message.getRecipientIds());
 	}
 
 	@Test
-	void testSetMessageText() {
-		fail("Not yet implemented");
+	public void testSetMessageText() {
+		String test_text = "How are you?";
+		test_message.setMessageText(test_text);
+		System.out.println("test setmessagetext" + test_message.getMessageText());
+		assertEquals(test_text, test_message.getMessageText());
 	}
 
 	@Test
-	void testSetTimeStamp() {
-		fail("Not yet implemented");
+	public void testSetTimeStamp() {
+		Date other_testdate = new Date();
+		test_message.setTimeStamp(other_testdate);
+		System.out.println("test settimestamp" + test_message.getTimeStamp());
+		assertEquals(other_testdate, test_message.getTimeStamp());
+		
 	}
 
 }
