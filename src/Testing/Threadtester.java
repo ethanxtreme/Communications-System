45ca592d7691129717a1d0b9cbc1482303043a9e
Samantha Bustamante <60456791+sam-24-bot@users.noreply.github.com>
2023-05-01 01:52:46 -0700

    Updated Thread tests

--- src/Testing/Threadtester.java
+++ src/Testing/Threadtester.java
@@ -1,40 +1,86 @@
 package Testing;
 import communicationsSystem.Thread;
+import communicationsSystem.ChatMessage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.Date;
+import java.util.*;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class Threadtester {
+	
+	Thread test_thread = new Thread();
 
 	@Test
-	void testThread() {
-		fail("Not yet implemented");
+	void testThread() { //default constructor
+		System.out.println("testThread number of threads" + test_thread.getNumMessages());
+		assertEquals(0,test_thread.getNumMessages());
 	}
 
 	@Test
-	void testThreadChatMessageArray() {
-		fail("Not yet implemented");
+	void testThreadConstructor() { //constructor
+		//tests if ChatMessage[] is appended to arraylist
+		boolean trueBool = true;
+		boolean falseBool = false;
+		ArrayList<ChatMessage> arr = new ArrayList<>();
+		ChatMessage[] messages = {};
+		for(int i = 0; i < messages.length; i++) {
+			arr.add(messages[i]);
+			if(arr.contains(messages[i])) {
+				assertTrue(trueBool);
+			}
+			else {
+				assertFalse(falseBool);
+			}
+		}
 	}
 
 	@Test
 	void testAddMessage() {
-		fail("Not yet implemented");
+		boolean trueBool = true;
+		boolean falseBool = false;
+		ArrayList<ChatMessage> hold_testmessage = new ArrayList<>();
+		String[] test_recipentIDs = {"a", "b", "c"};
+		Date test_date = new Date();
+		ChatMessage test_message = new ChatMessage("123", "efgh",test_recipentIDs,"Hello", test_date);
+		hold_testmessage.add(test_message);
+		if(hold_testmessage.contains(test_message)) {
+			assertTrue(trueBool);
+		}
+		else {
+			assertFalse(falseBool);
+		}
+		
 	}
 
 	@Test
 	void testGetNumMessages() {
-		fail("Not yet implemented");
+		assertEquals(0, test_thread.getNumMessages());
+		
 	}
 
 	@Test
 	void testGetThread() {
-		fail("Not yet implemented");
+		ArrayList<ChatMessage> hold_testmessage = new ArrayList<>();
+		String[] test_recipentIDs = {"a", "b", "c"};
+		Date test_date = new Date();
+		ChatMessage test_message = new ChatMessage("123", "efgh",test_recipentIDs,"Hello", test_date);
+		hold_testmessage.add(test_message);
+		assertEquals(hold_testmessage, test_thread.getThread());
+		
 	}
 
 	@Test
 	void testGetRecipientIds() {
-		fail("Not yet implemented");
+		String[] recipientids = {};
+		String content="";
+		for(int i = 0; i < recipientids.length; i++) {
+			content = recipientids[i];
+		}
+		assertEquals(recipientids, content);
 	}
 
 }
