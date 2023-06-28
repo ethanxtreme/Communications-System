0fb904e3f4339558a88d973f332e08bde863be59
Eakjot Hundal <eakjothundal@gmail.com>
2023-06-27 17:42:07 -0700

    Cleaned up some more code

--- src/communicationsSystem/GUI.java
+++ src/communicationsSystem/GUI.java
@@ -3,15 +3,13 @@ package communicationsSystem;
 
 // Communications system GUI 
 
-import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
-import java.io.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.lang.Thread;
+import java.util.Objects;
 
 
 // the only solution I think might work is connecting the GUI to the server so that it can have
@@ -28,8 +26,8 @@ public class GUI {
 	
 	public GUI(String window, boolean isAdmin, Client passedClient) {
 		
-		this.client = passedClient;
-		this.isAdmin = isAdmin;
+		client = passedClient;
+		GUI.isAdmin = isAdmin;
 		
 		if (window.equals("login")) {
 			createLoginWindow();
@@ -43,7 +41,7 @@ public class GUI {
 	private static void createLoginWindow() {    
 
 		JFrame frame = new JFrame("Log in");
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	    
 	    createLoginUI(frame);
 	    
@@ -53,11 +51,11 @@ public class GUI {
 	      
 	   }
 	
-	private static void createMessageWindow(boolean isAdmin) {
+	private static void createMessageWindow() {
 		JFrame frame = new JFrame("Messaging");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
-		createMessageUI(frame, isAdmin);	
+		createMessageUI(frame);
 		frame.setSize(800, 680);   
 	    frame.setLocationRelativeTo(null);  // Center on screen
 	    frame.setVisible(true);	// make visible
@@ -76,39 +74,35 @@ public class GUI {
 	    JButton exitButton = new JButton("Exit");
 	    
 	    // adding action listeners to buttons
-	    exitButton.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		JOptionPane.showMessageDialog(frame, "Quitting Application.");
-	    		System.exit(0);	// End program
-	        }
-	    });
+	    exitButton.addActionListener(e -> {
+			JOptionPane.showMessageDialog(frame, "Quitting Application.");
+			System.exit(0);	// End program
+		});
 	    
-	    loginButton.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		String name = usernameText.getText();
-				char[] passArray = passwordText.getPassword();
-				
-				// convert char[] to String
-				String pass = new String(passArray);
-				
-				// TODO debug message
+	    loginButton.addActionListener(e -> {
+			String name = usernameText.getText();
+			char[] passArray = passwordText.getPassword();
+
+			// convert char[] to String
+			String pass = new String(passArray);
+
+			// TODO debug message
 //				JOptionPane.showMessageDialog(frame, "Logging in with username -> password = " + name + " -> " + pass);
-				// TODO here, once a login function with username and password as parameters is
-				// created, just pass name and pass into the function
-				boolean success = client.login(name, pass);
-				username = name;
-	            if (success) {
-	            	// set variable to username entered
-	            	loggedInUser = usernameText.getText();
-	                JOptionPane.showMessageDialog(frame, "Login successful!");
-	                createMessageWindow(isAdmin);
-	                frame.dispose();
-	            } else {
-	                JOptionPane.showMessageDialog(frame, "Login failed. Please check your username and password.");
-	            }
-				
-	    	}
-	    });
+			// TODO here, once a login function with username and password as parameters is
+			// created, just pass name and pass into the function
+			boolean success = Client.login(name, pass);
+			username = name;
+			if (success) {
+				// set variable to username entered
+				loggedInUser = usernameText.getText();
+				JOptionPane.showMessageDialog(frame, "Login successful!");
+				createMessageWindow();
+				frame.dispose();
+			} else {
+				JOptionPane.showMessageDialog(frame, "Login failed. Please check your username and password.");
+			}
+
+		});
 	    
 	    
 	    
@@ -148,7 +142,7 @@ public class GUI {
 	}
 	
 	
-	private static void createMessageUI(final JFrame frame, boolean isAdmin) {
+	private static void createMessageUI(final JFrame frame) {
 		JPanel panel = new JPanel(new GridBagLayout());
 		
 		GridBagConstraints constraints = new GridBagConstraints();
@@ -195,10 +189,10 @@ public class GUI {
 	    String [] exampleUsers = {"Select a user to message:", "Jesse", "Samantha", "EJ", "Ethan", "Apurva"};
 	    String [] exampleGroups = {"Select a group conversation:", "Communication", "BlackJack", "Banking", "Class"};
 	    
-	    JComboBox convosComboBox = new JComboBox(exampleUsers);
+	    JComboBox<String> convosComboBox = new JComboBox<>(exampleUsers);
 //	    convosComboBox.setSelectedIndex(4);
 	    
-	    JComboBox groupsComboBox = new JComboBox(exampleGroups);
+	    JComboBox<String> groupsComboBox = new JComboBox<>(exampleGroups);
 	    
 	    // this will be the area where messages are shown
 	    JTextArea textArea = new JTextArea(5, 30);
@@ -214,141 +208,126 @@ public class GUI {
 	    
 	    JButton sendButton = new JButton("Send");
 	    
-	    selectButton.addActionListener(new ActionListener() {
-	    	public void actionPerformed(ActionEvent e) {
-	    		conversationLabel.setText("Currently messaging: " + exampleUsers[convosComboBox.getSelectedIndex()]);
-	    	}
-	    });
+	    selectButton.addActionListener(e -> conversationLabel.setText("Currently messaging: " + exampleUsers[convosComboBox.getSelectedIndex()]));
 	    
-	    sendButton.addActionListener(new ActionListener() {
-	    	@SuppressWarnings("deprecation")
-			public void actionPerformed(ActionEvent e) {
-	    		Date timestamp = new Date();
-	    		String time = timestamp.toString();
-	    		
-	    		// get the userID TODO hardcoded for now
-	    		User sender = new User("0001", loggedInUser, UserType.ADMIN, "1234abc");
-	    		User recipient = new User("0002", "ej", UserType.ADMIN, "12345");
-	    		
-	    		String [] recipientIds = {recipient.getId()};
-	    		
-	    		String messageId = "1";
-	    		
-	    		ChatMessage message = new ChatMessage(messageId, sender.getId(), recipientIds, messageText.getText(), timestamp);
-	    		
-	    		// TODO this only creates the message, not actually sends it
-	    		
-	    		// TODO in reality, this should put the message into the thread and then write the entire
-	    		// thread to the screen each time
-	    		if (messageText.getText().strip() != "") {
-	    			textArea.setText(textArea.getText() + timestamp.getHours() + ":" + timestamp.getMinutes() + " " +
-	    							sender.getUsername() + ": " + messageText.getText() + "\n");
-	    		}
-	    		// deletes the text inside the message text field
-	    		messageText.setText("");
-	    		
-	    		
-	            // Generate a response
-		        String responseText = "I'm busy right now. Can we talk later?";
+	    sendButton.addActionListener(e -> {
+			Date timestamp = new Date();
+			String time = timestamp.toString();
 
-		        // Get the name of the user who received the message
-		        String recipientName = (String) convosComboBox.getSelectedItem(); // TODO: replace with actual name
+			// get the userID TODO hardcoded for now
+			User sender = new User("0001", loggedInUser, UserType.ADMIN, "1234abc");
+			User recipient = new User("0002", "ej", UserType.ADMIN, "12345");
 
-		        // Create a new message object
-		        textArea.setText(textArea.getText() + timestamp.getHours() + ":" + timestamp.getMinutes() + 
-		        				" " + recipientName + ": "+responseText + "\n");
-		        //Message response = new Message(recipientName, responseText, new Date());
+			String [] recipientIds = {recipient.getId()};
 
-	           
-	            
-	    	}
-	    });
+			String messageId = "1";
+
+			ChatMessage message = new ChatMessage(messageId, sender.getId(), recipientIds, messageText.getText(), timestamp);
+
+			// TODO this only creates the message, not actually sends it
+
+			// TODO in reality, this should put the message into the thread and then write the entire
+			// thread to the screen each time
+			if (!Objects.equals(messageText.getText().strip(), "")) {
+				textArea.setText(textArea.getText() + timestamp.getHours() + ":" + timestamp.getMinutes() + " " +
+								sender.getUsername() + ": " + messageText.getText() + "\n");
+			}
+			// deletes the text inside the message text field
+			messageText.setText("");
+
+
+			// Generate a response
+			String responseText = "I'm busy right now. Can we talk later?";
+
+			// Get the name of the user who received the message
+			String recipientName = (String) convosComboBox.getSelectedItem(); // TODO: replace with actual name
+
+			// Create a new message object
+			textArea.setText(textArea.getText() + timestamp.getHours() + ":" + timestamp.getMinutes() +
+							" " + recipientName + ": "+responseText + "\n");
+			//Message response = new Message(recipientName, responseText, new Date());
+
+
+
+		});
 	    
-	    createGroupButton.addActionListener(new ActionListener() {
-	        @Override
-	        public void actionPerformed(ActionEvent e) {
-	        	JPanel panel = new JPanel(new GridBagLayout());
-	    		
-	    		GridBagConstraints constraints = new GridBagConstraints();
-	    	    constraints.anchor = GridBagConstraints.WEST;
-	    	    constraints.insets = new Insets(10, 10, 10, 10);
-	            JFrame frame = new JFrame("Create Group");
+	    createGroupButton.addActionListener(e -> {
+			JPanel panel1 = new JPanel(new GridBagLayout());
 
-	            JLabel groupLabel = new JLabel("Enter the name of the group:");
-	            JTextField groupTextField = new JTextField(20);
-	            JLabel label = new JLabel("Enter usernames of group members separated by commas:");
-	            JTextField textField = new JTextField(20);
-	            JButton button = new JButton("Create");
+			GridBagConstraints constraints1 = new GridBagConstraints();
+			constraints1.anchor = GridBagConstraints.WEST;
+			constraints1.insets = new Insets(10, 10, 10, 10);
+			JFrame frame1 = new JFrame("Create Group");
+
+			JLabel groupLabel = new JLabel("Enter the name of the group:");
+			JTextField groupTextField = new JTextField(20);
+			JLabel label = new JLabel("Enter usernames of group members separated by commas:");
+			JTextField textField = new JTextField(20);
+			JButton button = new JButton("Create");
 
 
-	            button.addActionListener(new ActionListener() {
-	                @Override
-	                public void actionPerformed(ActionEvent e) {
-	                    String groupName = groupTextField.getText();
-	                    String input = textField.getText();
-	                    //chatGroups.add(new ChatGroup(groupName, usernames)); // assuming groupList is a List<Group>
-	                    frame.dispose();
-	                }
-	            });
-	           
-	            
-	            constraints.gridx = 0;
-	            constraints.gridy = 0;
-	            panel.add(groupLabel, constraints);
-	            
-	            constraints.gridx = 0;
-	    	    constraints.gridy = 1;
-	            panel.add(groupTextField, constraints);
-	            
-	            constraints.gridx = 1;
-	    	    constraints.gridy = 0;
-	            panel.add(label, constraints);
-	            
-	            constraints.gridx = 1;
-	    	    constraints.gridy = 1;
-	            panel.add(textField, constraints);
-	            
-	            constraints.gridx = 2;
-	    	    constraints.gridy = 1;
-	            panel.add(button, constraints);          
-	            
-	            
-	            frame.getContentPane().add(panel, BorderLayout.CENTER);
-	            
-	            frame.setSize(800, 200);      
-	    	    frame.setLocationRelativeTo(null);  // Center on screen
-	    	    frame.setVisible(true);	// make visible
-	        }
-	    });
+			button.addActionListener(e1 -> {
+				String groupName = groupTextField.getText();
+				String input = textField.getText();
+				//chatGroups.add(new ChatGroup(groupName, usernames)); // assuming groupList is a List<Group>
+				frame1.dispose();
+			});
+
+
+			constraints1.gridx = 0;
+			constraints1.gridy = 0;
+			panel1.add(groupLabel, constraints1);
+
+			constraints1.gridx = 0;
+			constraints1.gridy = 1;
+			panel1.add(groupTextField, constraints1);
+
+			constraints1.gridx = 1;
+			constraints1.gridy = 0;
+			panel1.add(label, constraints1);
+
+			constraints1.gridx = 1;
+			constraints1.gridy = 1;
+			panel1.add(textField, constraints1);
+
+			constraints1.gridx = 2;
+			constraints1.gridy = 1;
+			panel1.add(button, constraints1);
+
+
+			frame1.getContentPane().add(panel1, BorderLayout.CENTER);
+
+			frame1.setSize(800, 200);
+			frame1.setLocationRelativeTo(null);  // Center on screen
+			frame1.setVisible(true);	// make visible
+		});
 	    
-	    viewLogsButton.addActionListener(new ActionListener() {
-	        public void actionPerformed(ActionEvent e) {
-	        	//create a JFrame to display the chat logs
-	        	JFrame logsFrame = new JFrame("Viewing All Chat Logs");
+	    viewLogsButton.addActionListener(e -> {
+			//create a JFrame to display the chat logs
+			JFrame logsFrame = new JFrame("Viewing All Chat Logs");
 
-	        	//create a JTextArea to hold the chat logs
-	        	JTextArea logsTextArea = new JTextArea();
-	        	logsTextArea.setEditable(false);
+			//create a JTextArea to hold the chat logs
+			JTextArea logsTextArea = new JTextArea();
+			logsTextArea.setEditable(false);
 
-	        	//add the logsTextArea to a JScrollPane to enable scrolling
-	        	JScrollPane logsScrollPane = new JScrollPane(logsTextArea);
+			//add the logsTextArea to a JScrollPane to enable scrolling
+			JScrollPane logsScrollPane = new JScrollPane(logsTextArea);
 
-	        	//add the logsScrollPane to the logsFrame
-	        	logsFrame.getContentPane().add(logsScrollPane);
+			//add the logsScrollPane to the logsFrame
+			logsFrame.getContentPane().add(logsScrollPane);
 
-	        	//set the size and visibility of the logsFrame
-	        	logsFrame.setSize(500, 500);
-	        	logsFrame.setVisible(true);
+			//set the size and visibility of the logsFrame
+			logsFrame.setSize(500, 500);
+			logsFrame.setVisible(true);
 
-	        	//add some example chat logs to the logsTextArea
-	        	logsTextArea.append("2023-05-01 10:15:00 Jesse: Hi, how are you?\n");
-	        	logsTextArea.append("2023-05-01 10:16:00 Samantha: I'm good, thanks for asking. How about you?\n");
-	        	logsTextArea.append("2023-05-01 10:17:00 Jesse: I'm doing well too. What have you been up to lately?\n");
-	        	logsTextArea.append("2023-05-01 10:18:00 Samantha: Not much, just working on some projects. What about you?\n");
-	        	logsTextArea.append("2023-05-01 10:19:00 Jesse: Same here, just trying to get some work done.\n");
+			//add some example chat logs to the logsTextArea
+			logsTextArea.append("2023-05-01 10:15:00 Jesse: Hi, how are you?\n");
+			logsTextArea.append("2023-05-01 10:16:00 Samantha: I'm good, thanks for asking. How about you?\n");
+			logsTextArea.append("2023-05-01 10:17:00 Jesse: I'm doing well too. What have you been up to lately?\n");
+			logsTextArea.append("2023-05-01 10:18:00 Samantha: Not much, just working on some projects. What about you?\n");
+			logsTextArea.append("2023-05-01 10:19:00 Jesse: Same here, just trying to get some work done.\n");
 
-	        }
-	    });
+		});
 
 	    constraints.gridx = 0;
 	    constraints.gridy = 0;
