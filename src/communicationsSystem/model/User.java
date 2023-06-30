4776a98ab1c83c2b839e885335ff313235bfda67
Eakjot Hundal <eakjothundal@gmail.com>
2023-06-29 22:41:47 -0700

    Started reworking tests and understanding them better

--- src/communicationsSystem/model/User.java
+++ src/communicationsSystem/model/User.java
@@ -3,96 +3,95 @@ package communicationsSystem.model;
 import java.util.ArrayList;
 
 public class User {
-	// Fields
-	private String userId;
-	private String username;
-	private UserType userType;
-	private String password;
-	private ArrayList<Thread> threads = new ArrayList<>();
-	
-	
-	// default constructor
-	public User() {
-		userId = "";
-		username = "";
-		userType = UserType.USER; 
-		password = "";
-	}
-	
-	public User(String userId, String username, UserType userType, String password) {
-		this.userId = userId;
-		this.username = username;
-		this.userType = userType;
-		this.password = password;
-	}
-	
-	// setters
-	public void setId(String id) {
-		userId = id;
-	}
-	
-	public void setUsername(String name) {
-		username = name;
-	}
-	
-	public void setType(UserType type) {
-		userType = type;
-	}
-	
-	public void setPassword(String pass) {
-		password = pass;
-	}
-	
-	public void addThread(Thread thread) {
-		threads.add(thread);
-	}
-	
-	
-	// getters
-	public String getId() {
-		return userId;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public UserType getType() {
-		return userType;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public ArrayList<Thread> getThreads() {
+    // Fields
+    private String userId;
+    private String username;
+    private UserType userType;
+    private String password;
+    private ArrayList<Thread> threads = new ArrayList<>();
+
+
+    // default constructor
+    public User() {
+        userId = "";
+        username = "";
+        userType = UserType.USER;
+        password = "";
+    }
+
+    public User(String userId, String username, UserType userType, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.userType = userType;
+        this.password = password;
+    }
+
+    public void addThread(Thread thread) {
+        threads.add(thread);
+    }
+
+    // getters
+    public String getId() {
+        return userId;
+    }
+
+    // setters
+    public void setId(String id) {
+        userId = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        username = name;
+    }
+
+    public UserType getType() {
+        return userType;
+    }
+
+    public void setType(UserType type) {
+        userType = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String pass) {
+        password = pass;
+    }
+
+    public ArrayList<Thread> getThreads() {
         return threads;
     }
-	
 
-	
-	
-	public void login(String password) {
-		
-		// TODO debug message
-		System.out.println("Login with password " + password);
-	}
-	
-	// return type may need to be NetworkMessage, but for now void seems to make more sense
-	public void message(ChatMessage message, String recipientUsername) { 
-		
-		// TODO debug message
-		System.out.println("Message reading \"" + message.getMessageText() + "\" sent to " + recipientUsername);
-	}
-	
-	// should only be accessible if user is an ADMIN
-	// for a GUI application, it's less necessary to code the UserType check in this class,
-	// but rather have the option only available on screen in user is an ADMIN
-	public void viewLogs() {
-		
-		// TODO debug message
-		System.out.println("View Logs successfully called.");
-	}
-	
-	
+
+    // TODO decide function/need of this method
+    public void login(String password) {
+        // debug message
+        System.out.println("Login with password " + password);
+    }
+
+    // TODO decide function/need of this method
+    // return type may need to be NetworkMessage, but for now void seems to make more sense
+    public void message(ChatMessage message, String recipientUsername) {
+
+        // debug message
+        System.out.println("Message reading \"" + message.getMessageText() + "\" sent to " + recipientUsername);
+    }
+
+    // TODO decide function/need of this method
+    // should only be accessible if user is an ADMIN
+    // for a GUI application, it's less necessary to code the UserType check in this class,
+    // but rather have the option only available on screen in user is an ADMIN
+    public void viewLogs() {
+
+        // TODO debug message
+        System.out.println("View Logs successfully called.");
+    }
+
+
 }
